4e5df9500d643ff7a62efbc6532c78ef1148245a
Duke <duke@datazip.io>
2025-12-26 23:54:09 +0530

    feat: add SSL/TLS configuration support for Kafka connections (#702)
    Signed-off-by: Sai Sanjay <saisanjay7660@gmail.com>
    Co-authored-by: Sai Sanjay <saisanjay7660@gmail.com>
    Co-authored-by: Ankit Sharma <111491139+hash-data@users.noreply.github.com>

--- utils/spec/uischema.go
+++ utils/spec/uischema.go
@@ -183,20 +183,53 @@ const KafkaUISchema = `{
     "ui:grid": [
       { "bootstrap_servers": 12, "consumer_group_id": 12 },
       { "threads_equal_total_partitions": 12, "max_threads": 12 },
-      { "protocol": 12, "backoff_retry_count": 12 }
+      { "backoff_retry_count": 12, "protocol": 12 }
     ],
     "protocol": {
       "ui:grid": [
-      { "sasl_mechanism": 12, "sasl_jaas_config": 12 }
+        { "sasl_mechanism": 12, "sasl_jaas_config": 12 },
+        { "tls_skip_verify": 24 },
+        { "ssl": 24 }
       ],
       "sasl_jaas_config": {
-      "ui:widget": "textarea",
+        "ui:widget": "textarea",
         "ui:options": {
           "rows": 1
         }
       },
+      "tls_skip_verify": {
+        "ui:widget": "boolean"
+      },
+      "ssl": {
+        "ui:options": {
+          "title": false,
+          "description": false
+        },
+        "ui:grid": [
+          { "server_ca": 12, "client_cert": 12 },
+          { "client_key": 12 }
+        ],
+        "server_ca": {
+          "ui:widget": "textarea",
+          "ui:options": {
+            "rows": 1
+          }
+        },
+        "client_cert": {
+          "ui:widget": "textarea",
+          "ui:options": {
+            "rows": 1
+          }
+        },
+        "client_key": {
+          "ui:widget": "textarea",
+          "ui:options": {
+            "rows": 1
+          }
+        }
+      },
       "ui:options": {
-      "title": false
+        "title": false
       }
     },
     "threads_equal_total_partitions": {
